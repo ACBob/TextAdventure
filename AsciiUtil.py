7d8d6783ef13a00b617ea1317d7e9c9ef0b82a69
ACBob <ethbot@hotmail.com>
2019-08-03 02:41:50 +0100

    Colors! And True Color is Supported aswell! Joy to the world!

--- AsciiUtil.py
+++ AsciiUtil.py
@@ -2,9 +2,9 @@ from PIL import Image
 import math
 import numpy as np
 
-pallette = " .:-=+*#%@"
+pallette = "@%#*+=-:. "
 
-def getAverageL(image):
+def getAverageL(image,TrueColor=False):
 
     im = np.array(image)
 
@@ -38,6 +38,10 @@ def openImageAsASCII(file):
     amig = []
 
     print(rows)
+    #SmolImage = image
+    #SmolImage.thumbnail((int(w),int(h)), Image.NEAREST)
+    
+    #SmolImage = SmolImage.load()
 
     for j in range(rows):
         y1=int(j*h)
@@ -52,21 +56,70 @@ def openImageAsASCII(file):
         for i in range(cols):
             x1 = int(i*w)
             x2 = int((i+1)*w)
-            print(i)
+            #print(i)
             #print(x1)
             #print(x2)
 
             #print(i)
             
             img = usedImage.crop((x1,y1,x2,y2))
+            imgColor = image.crop((x1,y1,x2,y2))
+            rgb=imgColor.convert('RGB')
+            #print(imgColor[0,0])
             #if j and i == 0: img.show()
+            
 
             avg = int(getAverageL(img))
 
             gsval = pallette[int((avg*9)/255)]
 
             #print(gsval)
+            #r='255'
+            #g='128'
+            #b='1'
+            
+            r,g,b = rgb.getpixel((0,0))
+            r,g,b = int(r),int(g),int(b)
+            print(r,g,b)
+            if not TrueColor:
+                #NOT TRUE COLOR.
+                if r > g and b:
+                    color='101' #Red
+                elif g > r and b:
+                    color='102' #Green
+                elif b > r and g:
+                    color='104' #Blue
+                #Mixes
+                elif r == b and r and b > g:
+                    color='45' #Magenta
+                elif r == g and r and g > b:
+                    color='43' #Orange
+                elif g == b and r and g > r:
+                    color='46' #Cyan
+                
 
-            amig[j] += gsval
+                #GrayScales.
+                elif r == b == g and r < 204 and r >128:
+                    color='47' #Gray
+                elif r == b == g and r >=204:
+                    color='107' #White
+                elif r == b == g and r<=128 and r>1:
+                    color='100' #Dark Gray
+                elif r == b == g and r <=1:
+                    color='40' #Black
+                else:
+                    print('color for column %s failed!'%(i))
+                    print(r)
+                    print(g)
+                    print(b)
+                    color='00'
+            else:
+                color=str(r)+';'+str(g)+';'+str(b)
+            
+            amig[j] += '\033['+'48;2;'+color+'m'+gsval+'\033[0m'
+    
+            #print(amig[j])
 
-    return amig
+    for i in amig:
+        print(i.replace('\n',''))
+    #return amig
