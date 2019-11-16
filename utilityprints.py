d95c6e4f3813c963f9d8a4ff22ead16090ee2642
ACBob <ethbot@hotmail.com>
2019-11-16 13:38:16 +0000

    Coloured Messages

--- utilityprints.py
+++ utilityprints.py
@@ -3,6 +3,6 @@ import shared
 import datetime
 
 def Info(text):
-    print("[Info]: "+text)
+    print("\u001b[33;1m[Info]: {}\u001b[0m".format(text))
 def Debug(text):
-    if shared.debug: print("%s{DEBUG} "%(str(datetime.datetime.now().time())[0:5])+text)
+    if shared.debug: print("\u001b[32;1m<Debug>: {}\u001b[0m".format(text))
